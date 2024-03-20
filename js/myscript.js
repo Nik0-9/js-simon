377e1630fa3bae8f73f95dc5f3ec0c7f09ace3ac
Nik0-9 <filannino.nicola9@gmail.com>
2024-03-20 16:07:36 +0100

    added eventListner on button create array of random number and print evry single number in a box on the html

--- js/myscript.js
+++ js/myscript.js
@@ -5,4 +5,25 @@ Da lì parte un timer di 30 secondi.
 Dopo i 30 secondi i numeri scompaiono e l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt() ( o meglio caselle di input).
 Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
  */
+let btnStart = document.getElementById('start');
 
+btnStart.addEventListener('click', function(){
+    let numberOfbox = 5
+    let randomArray = [];
+    randomArray = getRandomIntUnique(1, 100, numberOfbox);
+      console.log(randomArray);
+    const dispalyEl = document.getElementById('display');
+    dispalyEl.innerHTML ='';
+
+    for(i=0; i<numberOfbox; i++){
+        dispalyEl.appendChild(drawBox(randomArray[i]));
+    }
+
+})
+
+function drawBox(content){
+    let drawBox = document.createElement('span');
+    drawBox.classList.add('box');
+    drawBox.innerHTML = content;
+    return drawBox;
+}
--- js/utility.js
+++ js/utility.js
@@ -2,13 +2,13 @@ function getRndInteger(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function getRandomIntUnique(min, max,){
+function getRandomIntUnique(min, max, arrayLength){
   let uniqueInt;
   let list = [];
-      while(blacklist.length < 5){
+      while(list.length < arrayLength){
           uniqueInt = getRndInteger(min, max);
-          if(!blacklist.includes(uniqueInt)){
-              blacklist.push(uniqueInt);
+          if(!list.includes(uniqueInt)){
+              list.push(uniqueInt);
           }
       }
   return list;
